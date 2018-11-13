4672da12df782693caf14d36c199ab0aa658817a
kachu9875 <20171132@vision.hoseo.edu>
2018-11-13 12:17:27 +0900

    PP06.ImageSprite

--- GameFramework/PP01.HelloSDL/Game.cpp
+++ GameFramework/PP01.HelloSDL/Game.cpp
@@ -1,4 +1,5 @@
 #include "Game.h"
+#include <SDL_image.h>
 #include <iostream>
 
 using namespace std;
@@ -15,7 +16,10 @@ bool Game::init(const char*title, int xpos, int ypos, int width, int height, boo
 		}
 		m_bRunning = true;
 
-		SDL_Surface* pTempSurface = SDL_LoadBMP("assets/animate.bmp");
+		//SDL_Surface* pTempSurface = SDL_LoadBMP("assets/animate.bmp");
+		//SDL_Surface* pTempSurface = IMG_Load("assets/animate.png");
+		SDL_Surface* pTempSurface = IMG_Load("assets/animate-alpha.png");
+
 		m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
 		SDL_FreeSurface(pTempSurface);
 
@@ -26,6 +30,9 @@ bool Game::init(const char*title, int xpos, int ypos, int width, int height, boo
 		m_destinationRectangle.y = m_sourceRectangle.y = 0;
 		m_destinationRectangle.w = m_sourceRectangle.w;
 		m_destinationRectangle.h = m_sourceRectangle.h;
+
+		SDL_SetRenderDrawColor(m_pRenderer, 255, 0, 0, 255);
+
 	}
 	else {
 		return false;
