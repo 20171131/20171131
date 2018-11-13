d969220900a94882e42e88dab0df327c9ba689b6
kachu9875 <20171132@vision.hoseo.edu>
2018-11-13 12:34:18 +0900

    PP08.SingletoneTM

--- GameFramework/PP01.HelloSDL/Game.cpp
+++ GameFramework/PP01.HelloSDL/Game.cpp
@@ -16,7 +16,13 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 
 		m_bRunning = true;
 
-		m_textureManager.load("assets/animate-alpha.png", "animate", m_pRenderer);
+		// load 부분 대치   
+		if (!TheTextureManager::Instance()->load("assets/animate-alpha.png", "animate", m_pRenderer))
+		{
+			return false;
+		}
+
+		//m_textureManager.load("assets/animate-alpha.png", "animate", m_pRenderer);
 
 	}
 	else {
@@ -25,13 +31,20 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 	return true;
 }
 
+
 void Game::render()
 {
 	SDL_RenderClear(m_pRenderer);
-	m_textureManager.draw("animate", 0, 0, 128, 82, m_pRenderer);
-	m_textureManager.drawFrame("animate", 100, 100, 128, 82, 1, m_currentFrame, m_pRenderer);
+
+	// draw 부분 대치 
+	TheTextureManager::Instance()->draw("animate", 0, 0, 128, 82, m_pRenderer);
+
+	TheTextureManager::Instance()->drawFrame("animate", 100, 100, 128, 82, 1, m_currentFrame, m_pRenderer);
+
 	SDL_RenderPresent(m_pRenderer);
+
 }
+
 
 void Game::clean()
 {
