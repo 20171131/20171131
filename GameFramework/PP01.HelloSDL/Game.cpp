884fdaab4408641021b7e6d64da4811d38158bd3
kachu9875 <20171132@vision.hoseo.edu>
2018-11-13 12:24:51 +0900

    PP07.TextureManager

--- GameFramework/PP01.HelloSDL/Game.cpp
+++ GameFramework/PP01.HelloSDL/Game.cpp
@@ -4,47 +4,32 @@
 
 using namespace std;
 
-bool Game::init(const char*title, int xpos, int ypos, int width, int height, bool fullscreen)
+bool Game::init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen)
 {
 	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
 	{
 		m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, SDL_WINDOW_SHOWN);
-
 		if (m_pWindow != 0)
 		{
 			m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
 		}
+
 		m_bRunning = true;
 
-		//SDL_Surface* pTempSurface = SDL_LoadBMP("assets/animate.bmp");
-		//SDL_Surface* pTempSurface = IMG_Load("assets/animate.png");
-		SDL_Surface* pTempSurface = IMG_Load("assets/animate-alpha.png");
-
-		m_pTexture = SDL_CreateTextureFromSurface(m_pRenderer, pTempSurface);
-		SDL_FreeSurface(pTempSurface);
-
-		m_sourceRectangle.w = 128;
-		m_sourceRectangle.h = 82;
-
-		m_destinationRectangle.x = m_sourceRectangle.x = 0;
-		m_destinationRectangle.y = m_sourceRectangle.y = 0;
-		m_destinationRectangle.w = m_sourceRectangle.w;
-		m_destinationRectangle.h = m_sourceRectangle.h;
-
-		SDL_SetRenderDrawColor(m_pRenderer, 255, 0, 0, 255);
+		m_textureManager.load("assets/animate-alpha.png", "animate", m_pRenderer);
 
 	}
 	else {
-		return false;
+		return false; // sdl could not initialize
 	}
-
 	return true;
 }
 
 void Game::render()
 {
 	SDL_RenderClear(m_pRenderer);
-	SDL_RenderCopy(m_pRenderer, m_pTexture, &m_sourceRectangle, &m_destinationRectangle);
+	m_textureManager.draw("animate", 0, 0, 128, 82, m_pRenderer);
+	m_textureManager.drawFrame("animate", 100, 100, 128, 82, 1, m_currentFrame, m_pRenderer);
 	SDL_RenderPresent(m_pRenderer);
 }
 
@@ -74,5 +59,5 @@ void Game::handleEvents()
 
 void Game::update()
 {
-	m_sourceRectangle.x = 128 * int(((SDL_GetTicks() / 100) % 6));
+	m_currentFrame = int(((SDL_GetTicks() / 100) % 6));
 }
