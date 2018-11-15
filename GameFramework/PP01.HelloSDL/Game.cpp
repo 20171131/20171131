f6aef3969f6259e8284956e7075d9bc50a424e1c
kachu9875 <20171132@vision.hoseo.edu>
2018-11-15 16:23:20 +0900

    PP11.AbstractClass

--- GameFramework/PP01.HelloSDL/Game.cpp
+++ GameFramework/PP01.HelloSDL/Game.cpp
@@ -1,16 +1,18 @@
 #include "Game.h"
+#include "Player.h"
 #include "Enemy.h"
-#include "TextureManager.h"
 #include <SDL_image.h>
+#include "TextureManager.h"
 #include <iostream>
 
-using namespace std;
+Game* Game::s_pInstance = 0;
 
 bool Game::init(const char* title, int xpos, int ypos, int width, int height, bool fullscreen)
 {
 	if (SDL_Init(SDL_INIT_EVERYTHING) >= 0)
 	{
 		m_pWindow = SDL_CreateWindow(title, xpos, ypos, width, height, SDL_WINDOW_SHOWN);
+
 		if (m_pWindow != 0)
 		{
 			m_pRenderer = SDL_CreateRenderer(m_pWindow, -1, 0);
@@ -18,23 +20,13 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 
 		m_bRunning = true;
 
-		// load 부분 대치   
 		if (!TheTextureManager::Instance()->load("assets/animate-alpha.png", "animate", m_pRenderer))
 		{
 			return false;
 		}
 
-		m_go = new GameObject();
-		m_player = new Player();
-		m_enemy = new Enemy();
-
-		m_go->load(100, 100, 128, 82, "animate");
-		m_player->load(300, 300, 128, 82, "animate");
-		m_enemy->load(0, 0, 128, 82, "animate");
-
-		m_gameObject.push_back(m_go);
-		m_gameObject.push_back(m_player);
-		m_gameObject.push_back(m_enemy);
+		m_gameObjects.push_back(new Player(new LoaderParams(100, 100, 128, 82, "animate")));
+		m_gameObjects.push_back(new Enemy(new LoaderParams(300, 300, 128, 82, "animate")));
 
 	}
 	else {
@@ -43,20 +35,19 @@ bool Game::init(const char* title, int xpos, int ypos, int width, int height, bo
 	return true;
 }
 
-
 void Game::render()
 {
-	SDL_RenderClear(m_pRenderer); // clear to the draw colour
-	for (std::vector<GameObject*>::size_type i = 0; i != m_gameObject.size(); i++)
+	SDL_RenderClear(m_pRenderer); // draw colour로 지움
+	for (std::vector<GameObject*>::size_type i = 0; i != m_gameObjects.size(); i++)
 	{
-		m_gameObject[i]->draw(m_pRenderer);
+		m_gameObjects[i]->draw();
 	}
 	SDL_RenderPresent(m_pRenderer); // draw to the screen
 }
 
 void Game::clean()
 {
-	std::cout << "cleaning game" << endl;
+	std::cout << "cleaning game" << std::endl;
 	SDL_DestroyWindow(m_pWindow);
 	SDL_DestroyRenderer(m_pRenderer);
 	SDL_Quit();
@@ -80,8 +71,9 @@ void Game::handleEvents()
 
 void Game::update()
 {
-	for (std::vector<GameObject*>::size_type i = 0; i != m_gameObject.size(); i++)
+	for (std::vector<GameObject*>::size_type i = 0; i != m_gameObjects.size(); i++)
 	{
-		m_gameObject[i]->update();
+		m_gameObjects[i]->update();
 	}
 }
+
--- GameFramework/PP01.HelloSDL/GameObject.h
+++ GameFramework/PP01.HelloSDL/GameObject.h
@@ -1,16 +1,19 @@
 #pragma once
+#include "LoaderParams.h"
 #include "SDL.h"
 #include <map>
 
 class GameObject
 {
 public:
-	virtual void load(int x, int y, int width, int height, std::string textureID);
-	virtual void draw(SDL_Renderer* pRenderer);
-	virtual void update();
-	virtual void clean();
+	virtual void draw() = 0;
+	virtual void update() = 0;
+	virtual void clean() = 0;
 
 protected:
+	GameObject(const LoaderParams* pParams) {}
+	virtual ~GameObject() {}
+
 	std::string m_textureID;
 	int m_currentFrame;
 	int m_currentRow;
@@ -18,4 +21,7 @@ protected:
 	int m_y;
 	int m_width;
 	int m_height;
+	int a = 1;
+	int num;
 };
+
