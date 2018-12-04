2f6ec00debb6eb7299cd8e9fd99ba37edc18a9bf
kachu9875 <20171132@vision.hoseo.edu>
2018-12-04 13:18:03 +0900

    미니게임 수정

--- GameFramework/PP01.HelloSDL/PlayState.cpp
+++ GameFramework/PP01.HelloSDL/PlayState.cpp
@@ -12,11 +12,6 @@ const std::string PlayState::s_playID = "PLAY";
 
 void PlayState::update()
 {
-	if (TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_ESCAPE))
-	{
-		TheGame::Instance()->getStateMachine()->changeState(new PauseState());
-	}
-
 	for (int i = 0; i < m_gameObjects.size(); i++) 
 	{
 		m_gameObjects[i]->update();
@@ -25,6 +20,11 @@ void PlayState::update()
 	if (checkCollision(dynamic_cast<SDLGameObject*>(m_gameObjects[0]), dynamic_cast<SDLGameObject*>(m_gameObjects[1])))
 	{
 		TheGame::Instance()->getStateMachine()->changeState(new GameOverState());
+	}
+
+	if (TheInputHandler::Instance()->isKeyDown(SDL_SCANCODE_ESCAPE))
+	{
+		TheGame::Instance()->getStateMachine()->changeState(new PauseState());
 	}
 }
 
